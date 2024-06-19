2de45ab86123b4e5616f51ea3fb63b6fb228662e
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 12:35:44 +0900

    Initial Commit

---
+++ BindingPractice/BindingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingPracticeApp.swift
+//  BindingPractice
+//
+//  Created by Brandon Jones on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BindingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
